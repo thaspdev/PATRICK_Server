107ea8cded0887807cfae0bbcfe17f17f736a157
thaspdev <thaspdev@gmail.com>
2016-12-05 19:49:36 +0100

    Initial commit

---
+++ sharedParts/connectDB.php
@@ -0,0 +1,10 @@
+<?php
+try {
+  $DB = new PDO('mysql:host=localhost;dbname=HWP_DB;charset=utf8','root','');
+} catch (Exception $e) {
+  die('Error : ' . $e->getMessage());
+}
+if (!isset($_COOKIE['PATRICK_cookie_consent'])) {
+  setcookie("HWP_cookie_consent", "yes", time()+24*60*60*365, null, null, false, true);//Cookie consent
+}
+?>
